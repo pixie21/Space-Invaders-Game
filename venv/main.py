e5b18d83c319bedfbd5cce8d6eb1a1aa6f48a94e
pixie21 <49859749+pixie21@users.noreply.github.com>
2020-07-25 21:19:33 -0500

    Score Added

--- venv/main.py
+++ venv/main.py
@@ -25,15 +25,23 @@ playerY = 480
 playerX_change = 0
 playerY_change = 0
 
-score = 0
+
 
 # Player/Enemy
-enemyImg = pygame.image.load('enemy.png')
-# Player dimentions on start. Centered and to the bottom
-enemyX = random.randint(0, 736)
-enemyY = random.randint(50, 150)
-enemyX_change = 4
-enemyY_change = 40
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    # Player dimentions on start. Centered and to the bottom
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(4)
+    enemyY_change.append(40)
 
 # Bullet
 bulletImg = pygame.image.load('bullet.png')
@@ -46,13 +54,26 @@ bulletY_change = 10
 bullet_state = "ready"
 
 
+# Score
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+textX = 10
+textY = 10
+
+#show the score
+
+def showScore(x, y):
+    score = font.render("Score:" + " " + str(score_value), True, (255,255,255))
+    screen.blit(score, (x, y))
+
 # draw an image of player on the screen
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
 
 
 def fireBullet(x, y):
@@ -104,23 +125,36 @@ while running:
                 playerY_change = 0
 
     # update the player coordinates
-    playerX += playerX_change
+
     playerY += playerY_change
-    enemyX += enemyX_change
 
     # confining the spaceship to the width of the screen
+    playerX += playerX_change
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
 
     # confining the enemy to the width of the screen
-    if enemyX <= 0:
-        enemyX_change = 4
-        enemyY += enemyY_change
-    elif enemyX >= 736:
-        enemyX_change = -4
-        enemyY += enemyY_change
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 4
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -4
+            enemyY[i] += enemyY_change[i]
+
+        # Collision
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 736)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
 
     # bullet movement
     if bulletY <= 0:
@@ -131,18 +165,11 @@ while running:
         fireBullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
-    #Collision
-    collision = isCollision(enemyX,enemyY,bulletX,bulletY)
-    if collision:
-        bulletY = 480
-        bullet_state= "ready"
-        score += 1
-        enemyY = random.randint(0,736)
-        enemyY = random.randint(50,150)
-
-
     # calling player image on top of the screen image
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+
+    #SHOW SCORE
+    showScore(textX,textY)
+
     # updating the screen to display icons and screen
     pygame.display.update()
